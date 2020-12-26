2e4f758e9214e58f6c1c6ee00a19f755be392f44
assefa1122 <assefademekebrihanu1122@gmail.com>
2020-12-26 15:39:08 -0500

    ASSIGNMENT
    NAME            ID
    Assefa demeke   1122
    ashenafi tesfay  1118
    
     Assignment one answer
     1,C
     2, D
     3, B
     4, D
     5, B & C
     Assignment two
     1, B
     2, C
     3, A
     4, A

---
+++ app/src/main/java/com/example/assignmentone/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.assignmentone;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.assignmentone.R;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.assignmentone.MESSAGE";
+    private int count1=0;
+    private Button button1;
+    private Button button2;
+    private TextView textView1;
+    private String toast1;
+    private Intent  intent;
+
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1=findViewById(R.id.textView1);
+
+    }
+
+    public void sayHello(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        String message = textView1.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+
+
+        //Toast toast=Toast.makeText((this,"HELLO:"+textView1.getText()));
+        Toast.makeText(this, "HELLO:"+textView1.getText(), Toast.LENGTH_LONG).show();
+    }
+
+    public void count(View view) {
+        ++count1;
+        if(textView1.getText()!=null)
+        {
+            textView1.setText(Integer.toString(count1));
+        }
+    }
+}
